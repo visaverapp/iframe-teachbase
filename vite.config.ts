f06e6979821d075280fb7b61ae444ade661bba08
KirylHritsiuk <pishime2017@gmail.com>
2024-11-25 21:32:42 +0300

    add settings

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import {defineConfig} from "vite";
+import { defineConfig } from "vite";
 import react from '@vitejs/plugin-react';
 import svgrPlugin from 'vite-plugin-svgr';
 import viteTsconfigPaths from 'vite-tsconfig-paths';
@@ -11,7 +11,7 @@ export default defineConfig({
     viteTsconfigPaths(),
     svgrPlugin(),
   ],
-  // base: "/iframe-teachbase/",
+  base: '/iframe-teachbase/',
   build: {
     outDir: 'build',
     sourcemap: true,
