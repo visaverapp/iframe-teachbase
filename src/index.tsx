1c174d4995e3c2a7ddb0e17deb264362f69da583
KirylHritsiuk <pishime2017@gmail.com>
2024-11-25 21:58:08 +0300

    check env

--- src/index.tsx
+++ src/index.tsx
@@ -4,18 +4,28 @@ import App from './App';
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "@/store/store";
+import { ErrorBoundary } from 'react-error-boundary';
+
+console.log({VITE_BASE_URL: import.meta.env.VITE_BASE_URL});
 
 const rootElement = document.getElementById('root');
+
 
 if (rootElement) {
   const root = ReactDOM.createRoot(rootElement);
   root.render(
       <StrictMode>
+           <ErrorBoundary
+          fallback={
+            <div>Упс что-то пошло не так</div>}
+        >
+
         <Provider store={store}>
           <BrowserRouter>
             <App />
           </BrowserRouter>
         </Provider>
+        </ErrorBoundary>
       </StrictMode>
   );
 } else {
